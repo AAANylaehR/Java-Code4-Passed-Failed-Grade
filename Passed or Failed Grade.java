6acdd900ab8fa24c4a6551faa246885fa0883c02
EsclaresRhea <142650868+AAANylaehR@users.noreply.github.com>
2023-10-21 10:46:17 +0800

    Passed or Failed Grade.java

--- Passed or Failed Grade.java
+++ Passed or Failed Grade.java
@@ -2,6 +2,13 @@ package Esclareslab3;
 
 public class Grade {
 
-public static void main(String[] args) { String result = ""; int grade = 87 ; grade = 87 ; result= (grade > 75)? "PASSED" : "Failed"; System.out.println("Your grade is 87"); System.out.println("The result is " + result);
+public static void main(String[] args) { 
+  String result = ""; 
+  int grade = 87 ; 
+  grade = 87 ; 
+  result= (grade > 75)? "PASSED" : "Failed"; 
+  System.out.println("Your grade is 87"); 
+  System.out.println("The result is " + result);
 
-} }
+}
+}
